2f318e2b7082c40ceb9042260f7fa45fef1fd932
aman khan <mohdamankhan18@gmail.com>
2024-03-09 13:16:14 +0530

    jkbhj

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import All from "./components/All";
+// import { Element } from 'react-scroll';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import ManagerLogin from "./components/ManagerLogin";
+import ManagerSignup from "./components/ManagerSignup";
+import EmployeeLogin from "./components/EmployeeLogin";
+import EmployeeSignup from "./components/EmployeeSignup";
+
+import "./App.css";
+import AdminDashboard from "./components/Manager/AdminDashboard";
+import EmployeeDashboard from "./components/Employee/EmployeeDashboard";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<All />} />
+          <Route path="/login" element={<ManagerLogin />} />
+          <Route path="/signup" element={<ManagerSignup />} />
+          <Route path="/elogin" element={<EmployeeLogin />} />
+          <Route path="/esignup" element={<EmployeeSignup />} />
+          
+          <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+          <Route path="/manager-dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
